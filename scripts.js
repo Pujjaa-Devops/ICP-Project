e76afedd6cdb2f009362ff95495a17382fe30029
Pujjaa-Devops <rautpooja0687@outlook.com>
2024-12-02 16:05:56 +0530

    about-page edit

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    navbar.classList.remove('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
